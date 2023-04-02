72208ff02c748ae182e4e357ea8abd11c4fe5cce
Hannes Hofer <Hannes.Hofer@gmail.com>
2023-04-02 20:12:52 +0200

    add long_description

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup
-import fenrir
+import fenrircore
 
 setup(
-    name='fenrir',
-    version=fenrir.__version__,
-    long_description=__doc__,
+    name='fenrircore',
+    version=fenrircore.__version__,
     description='use Fenrir to route configured network devices trough configured VPN tunnel',
+    long_description="""Fenrir provides a user friendly way to route all traffic from configured trough a VPN tunnel.
+
+This is done via ARP Spoofing. Determined default GW on inputinterface is spoofed to configured device.
+    """,
     authors=['Hannes Hofer <Hannes.Hofer@gmail.com>'],
-    packages=['fenrir'],
+    packages=['fenrircore'],
     include_package_data=True,
     zip_safe=False,
     install_requires=['cryptography', 'pyroute2', 'python_iptables', 'requests', 'scapy'],
-    entry_points={'console_scripts': ['fenrir = fenrir.fenrir:main', ]},
+    entry_points={'console_scripts': ['fenrir = fenrircore.fenrircore:main', ]},
     project_urls={
         'Source': 'https://github.com/HannesHofer/fenrir',
         'Tracker': 'https://github.com/HannesHofer/fenrir/issues'

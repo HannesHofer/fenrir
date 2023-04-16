747be9853c577364d19b699fe11c494bf585dab9
Hannes Hofer <Hannes.Hofer@gmail.com>
2023-04-16 12:04:39 +0200

    bump version number

--- tests/fenrir_test.py
+++ tests/fenrir_test.py
@@ -8,7 +8,7 @@ class FenrirTestCase(TestCase):
         self.fenrir = fenrir.Fenrir(inputinterface='eth0', vpninterface='tun0', dbpath='/tmp/fenrir.db', password=None)
 
     def test_version(self):
-        assert __version__ == '0.6.0'
+        assert __version__ == '0.7.0'
 
     def setup(self):
         with patch('fenrircore.fenrir.makedirs') as mkdirmock, \
